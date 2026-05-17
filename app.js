c2a05389d2a0550b52954a1eb3b7870dd6b5bcab
Siva M <murugansiva1260@gmail.com>
2026-05-18 00:37:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Jenkins CI/CD Pipeline Wo0rking!');
+    res.send('Jenkins CI/CD Pipeline Wo0rkiing!');
 });
 
 app.listen(3000, () => {
